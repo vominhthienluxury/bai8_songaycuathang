9a594b7e3b7a2b8bbb8cedc8935561b20b9c23f8
vominhthienluxury <thanhsooon1111@gmail.com>
2024-11-11 14:47:35 +0700

    Add project files.

---
+++ bai8_songaycuathang.cpp
@@ -0,0 +1,51 @@
+// bai8_songaycuathang.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    int thang;
+    cout << "nhap thang:  ";
+    cin >> thang;
+    switch (thang)
+    {
+    case 1:
+        cout << "\nco 31 ngay";
+        break;
+    case 2:
+        cout << "\nco 28 ngay";
+        break;
+    case 3:
+        cout << "\nco 31 ngay";
+        break;
+    case 4:
+        cout << "\nco 30 ngay";
+        break;
+    case 5:
+        cout << "\nco 31 ngay";
+        break;
+    case 6:
+        cout << "\nco 30 ngay";
+        break;
+    case 7:
+        cout << "\nco 31 ngay";
+        break;
+    case 8:
+        cout << "\nco 31 ngay";
+        break;
+    case 9:
+        cout << "\nco 30 ngay";
+        break;
+    case 10:
+        cout << "\nco 31 ngay";
+        break;
+    case 11:
+        cout << "\nco 30 ngay";
+        break;
+    case 12:
+        cout << "\nco 31 ngay";
+        break;
+        return 0;
+    }
+}
